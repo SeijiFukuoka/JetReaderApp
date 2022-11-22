daa1f3b438f9e99e59aa66f6bebdb657f4c1a5e5
Seiji <seiji@skeelo.app>
2022-11-21 21:12:43 -0300

    reader splash screen

--- JetReaderApp2/app/src/main/java/br/com/jetreaderapp/screens/login/ReaderLoginScreen.kt
+++ JetReaderApp2/app/src/main/java/br/com/jetreaderapp/screens/login/ReaderLoginScreen.kt
@@ -1,8 +1,10 @@
 package br.com.jetreaderapp.screens.login
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
 @Composable
 fun ReaderLoginScreen(navController: NavHostController) {
+    Text(text = "Login")
 }
